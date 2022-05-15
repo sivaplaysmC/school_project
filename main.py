c569f82ae92f854b0faf39f1dc635b08af27c171
siva <sivaram.asdf@gmail.com>
2022-05-15 13:28:14 +0530

    Nothing interesting

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ import pygame
 pygame.init()
 
 win = pygame.display.set_mode((800,600))
+
+while run :
+    for event in pygame.event.get :
+        if event.type == pygame.QUIT :
+            run = False
