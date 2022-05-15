1cd30252f80f457a3da21a6f74d4ae9155b4e12b
siva <sivaram.asdf@gmail.com>
2022-05-15 21:33:16 +0530

    adding y component ._.

--- acceleration.py
+++ acceleration.py
@@ -5,38 +5,44 @@ win = pygame.display.set_mode((1280,720))
 clock = pygame.time.Clock()
 framerate = 60
 
-acceleration = 0
-velocity = 0
-position = 10
+x_acceleration = 0
+x_velocity = 0
+x_position = 10
+
+y_acceleration = 9.8
+y_velocity = 0
+y_position = 360
 
 is_jumping = False
 
-rect = pygame.Rect(position,360,50,50)
+rect = pygame.Rect(x_position,360,50,50)
 while True:
-    rect.move_ip(0,10)
+    y_position += 1
+
+    rect.update(0,10)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
     clock.tick(framerate)
-    acceleration = 0
+    x_acceleration = 0
     keys = pygame.key.get_pressed()
     if keys[pygame.K_RIGHT]:
-        acceleration += 1
+        x_acceleration += 1
     if keys[pygame.K_LEFT] :
-        acceleration -= 1
+        x_acceleration -= 1
     if not is_jumping :
         if keys[pygame.K_SPACE] :
             rect.move_ip(0,1)
             is_jumping = True
         else :
             is_jumping = False
-    # if abs(acceleration) > 10 :
-    acceleration += velocity * -0.025
-    velocity += acceleration
-    position += velocity + 0.5 * acceleration
-    rect.update(position , 360 , 50 , 50 )
-    
+    # if abs(x_acceleration) > 10 :
+    x_acceleration += x_velocity * -0.025
+    x_velocity += x_acceleration
+    x_position += x_velocity + 0.5 * x_acceleration
+    rect.update(x_position , 360 , 50 , 50 )
+
     rect.midbottom = rect.topleft
     print(rect.y)
     win.fill('white')
