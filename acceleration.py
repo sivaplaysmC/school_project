ecea73a032c097dac16fec96f723c1199676cc90
siva <sivaram.asdf@gmail.com>
2022-05-16 11:42:05 +0530

    commiting using git gui

--- acceleration.py
+++ acceleration.py
@@ -19,7 +19,7 @@ rect = pygame.Rect(x_position,360,50,50)
 while True:
     y_position += 1
 
-    rect.update(0,10)
+    # rect.update(0,y_position , 50 , 50 )
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -31,20 +31,29 @@ while True:
         x_acceleration += 1
     if keys[pygame.K_LEFT] :
         x_acceleration -= 1
-    if not is_jumping :
-        if keys[pygame.K_SPACE] :
-            rect.move_ip(0,1)
-            is_jumping = True
-        else :
-            is_jumping = False
+    # if not is_jumping :
+    if keys[pygame.K_SPACE] :
+        rect.move_ip(0,1000)
+        is_jumping = True
+    else :
+        is_jumping = False
     # if abs(x_acceleration) > 10 :
     x_acceleration += x_velocity * -0.025
     x_velocity += x_acceleration
     x_position += x_velocity + 0.5 * x_acceleration
-    rect.update(x_position , 360 , 50 , 50 )
+    rect.update(x_position , y_position , 50 , 50 )
+    if y_position > 710 :
+        y_position = 710
+        # rect.update(rect.x , 710, rect.w , rect.h)
+    else :
+        y_acceleration += y_velocity * -0.025
+        y_velocity += y_acceleration
+        y_position += y_velocity + 0.5 * y_acceleration
+        rect.update(x_position , y_position , 50 , 50 )
+
 
     rect.midbottom = rect.topleft
-    print(rect.y)
+    print(rect.y , y_position)
     win.fill('white')
     pygame.draw.rect(win,'red',rect)
     pygame.display.flip()
