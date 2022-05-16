d96adcbd028b12da034d741631969689956de0f7
sivaplaysmC <sivaram.asdf@gmail.com>
2022-05-16 16:04:05 +0530

    haha i am so bored

--- acceleration_but_2_dimensional.py
+++ acceleration_but_2_dimensional.py
@@ -1,31 +1,36 @@
 import pygame , sys
-import pygame.math.Vector2 as vec
 pygame.init()
 
 win = pygame.display.set_mode((1280,720))
-
-obj = pygame.Surface(50,50)
+clock = pygame.time.Clock()
+obj = pygame.Surface((50,50))
 obj.fill("blue")
 
-run = 1
+run =1
 
-acceleration  = vec(0,0)
-velocity = vec(0,0)
-position = vec(100,100)
+acceleration  = [0,0]
+velocity = [0,0]
+position = [100,100]
 
 
 
 
 while run :
+    dt = clock.tick(60) / 1000
     for event in pygame.event.get() :
         if event.type == pygame.QUIT :
             run = 0
             sys.exit()
-    
+    keys = pygame.key.get_pressed()  
     if keys[pygame.K_RIGHT] :
-        acceleration.x += 1
-    if keys[]
+        accelertion.x += 1
+    if keys[pygame.K_LEFT] :
+        acceleration.x -= 1 
 
+    acceleration = [0,0.5]
+    accelereation = acceleration - velocity
+    velocity = velocity + acceleration *dt
+    position = position + velocity * dt + ( acceleration * 0.5 ) * dt * dt
 
     win.fill((255,255,255))
     win.blit(obj , position)
