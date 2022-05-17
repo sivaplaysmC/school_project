ca97e21be6d232cf5391ea6eb639ec07d94885c5
sivaplaysmC <sivaram.asdf@gmail.com>
2022-05-17 12:40:42 +0530

    nothing jere

--- acceleration_but_2_dimensional.py
+++ acceleration_but_2_dimensional.py
@@ -8,9 +8,9 @@ obj.fill("blue")
 
 run =1
 
-acceleration  = [0,0]
-velocity = [0,0]
-position = [100,100]
+acceleration  = pygame.math.Vector2([0,0])
+velocity = pygame.math.Vector2([0,0])
+position = pygame.math.Vector2([100,100])
 
 
 
@@ -21,14 +21,14 @@ while run :
         if event.type == pygame.QUIT :
             run = 0
             sys.exit()
-    keys = pygame.key.get_pressed()  
+    keys = pygame.key.get_pressed()
     if keys[pygame.K_RIGHT] :
-        accelertion.x += 1
+        acceleration.x += 1
     if keys[pygame.K_LEFT] :
-        acceleration.x -= 1 
+        acceleration.x -= 1
 
-    acceleration = [0,0.5]
-    accelereation = acceleration - velocity
+    acceleration = pygame.math.Vector2(0,0.5)
+    acceleration.x = acceleration.x - 0.001
     velocity = velocity + acceleration *dt
     position = position + velocity * dt + ( acceleration * 0.5 ) * dt * dt
 
