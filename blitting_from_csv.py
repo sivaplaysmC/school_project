e97e2dee0ec9145b37184ade349d9de2759aa33b
sivaplaysmC <sivaram.asdf@gmail.com>
2022-05-20 15:46:36 +0530

    added background image

--- blitting_from_csv.py
+++ blitting_from_csv.py
@@ -4,15 +4,17 @@ from map_parser import main
 
 pygame.init()
 
-win = pygame.display.set_mode((160,160))
+win = pygame.display.set_mode((480,320))
 
 clock = pygame.time.Clock()
-im1 = pygame.image.load(r"G:\school_project\split_tiles\tile029.png").convert_alpha()
-im2 = pygame.image.load(r"G:\school_project\split_tiles\tile007.png")
+bg = pygame.image.load(r"assets\Background\single_background.png").convert_alpha()
+bg = pygame.transform.scale(bg , (480,320))
+# im1 = pygame.image.load(r"split_tiles\tile029.png").convert_alpha()
+# im2 = pygame.image.load(r"split_tiles\tile007.png")
 blitter_x = 0
 blitter_y = 0
 run = True
-
+win.blit(bg , (0,0))
 x = main()
 for i in x :
     blitter_x = 0
@@ -20,9 +22,9 @@ for i in x :
         print(blitter_x , blitter_y)
         if j != -1 :
             if j < 10 :
-                win.blit(pygame.image.load(r"G:\school_project\split_tiles\tile00"+str(j)+".png").convert_alpha() , (blitter_x , blitter_y))
+                win.blit(pygame.image.load(r"split_tiles\tile00"+str(j)+".png").convert_alpha() , (blitter_x , blitter_y))
             elif j < 100 :
-                win.blit(pygame.image.load(r"G:\school_project\split_tiles\tile0"+str(j)+".png").convert_alpha() , (blitter_x , blitter_y))
+                win.blit(pygame.image.load(r"split_tiles\tile0"+str(j)+".png").convert_alpha() , (blitter_x , blitter_y))
         blitter_x += 16
     blitter_y += 16
 while run :
