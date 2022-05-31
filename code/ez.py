2b25c28a6565172be2330a0900c1684a72da0d73
sivaplaysmC <sivaram.asdf@gmail.com>
2022-05-31 16:19:02 +0530

    hha

--- code/ez.py
+++ code/ez.py
@@ -6,10 +6,14 @@ win = pygame.display.set_mode((1280,720))
 
 run = True
 
+rect = pygame.Rect(100, 200, 50, 50)
 while run == True :
     for event in pygame.event.get() :
         # print(event)
         if event.type == pygame.QUIT :
             run = False
     win.fill((0,0,153))
+    pygame.draw.rect(win, (250,250,250), rect)
     pygame.display.update()
+
+
