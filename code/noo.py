a843cd10637095d59213782c956dc2112fabb509
sivaplaysmC <root@RamPC.localdomain>
2022-06-05 13:40:55 +0530

    cleared all junk

--- code/noo.py
+++
@@ -1,4 +0,0 @@
-a = int(input("Enter a number"))
-
-if a > 2 :
-    print("Hi There I am Sivaramakrishnan")
--- tkinte.py
+++
@@ -1,6 +0,0 @@
-import tkinter as tk
-
-win = tk.Tk()
-win.config(width=800,height=800)
-win.mainloop()
-
