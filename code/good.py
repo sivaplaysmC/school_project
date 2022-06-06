cfe40ea010ca4fb6649a789fd265612258b064c5
sivaplaysmC <sivaram.asdf@gmail.com>
2022-06-06 18:50:26 +0530

    Added a dumb gaem pause and two players !!

--- code/good.py
+++ code/good.py
@@ -17,7 +17,21 @@ border = pygame.Rect(0,0,WIDTH , HEIGHT)
 class border(pygame.Rect) :
     def __init__(self , x , y , w , h ) :
         pass
+paused = False
+def pause() :
+    global paused , run
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            paused = True
 
+    while paused :
+        for event in pygame.event.get() :
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                paused = False
+            if event.type == pygame.QUIT :
+                paused = False
+                run = False
+        win.fill("black")
 
 class Platform(pygame.sprite.Sprite) :
     def __init__(self , x , y , w , h , color):
@@ -29,11 +43,17 @@ class Platform(pygame.sprite.Sprite) :
         self.rect.update(x , y , w , h )
 
 class Entity(pygame.sprite.Sprite) :
-    def __init__(self):
+    def __init__(self , color):
         super().__init__()
+        self.color = color
         self.image = pygame.Surface((50,50))
-        self.image.fill((0,55,255))
+        self.image.fill(self.color)
         self.px , self.py = 0 , 0 
+        
+
+        self.move_right_key = pygame.K_RIGHT
+        self.move_left_key  = pygame.K_LEFT
+        self.jump_key = pygame.K_SPACE
 
         self.rect = self.image.get_rect()
         self.rect.x = 500
@@ -54,13 +74,12 @@ class Entity(pygame.sprite.Sprite) :
         self.acceleration.x , self.acceleration.y =  0 , 0
         # self.delpos.x , self.delpos.y  = 0 , 0 
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_RIGHT] :
-            self.acceleration.x += 2
-        if keys[pygame.K_LEFT] :
-            self.acceleration.x -= 2
-        
-        if keys[pygame.K_SPACE] and self.velocity.y == 0 :
-            print("colliderect")
+        if keys[self.move_right_key] :
+            self.acceleration.x += 4
+        if keys[self.move_left_key] :
+            self.acceleration.x -= 4
+        if keys[self.jump_key] and self.velocity.y == 0 :
+            # print("colliderect")
             self.velocity.y -= 9
 
     def horizontal_movement(self) :
@@ -69,7 +88,7 @@ class Entity(pygame.sprite.Sprite) :
         self.delpos.x = self.velocity.x *dt + ((self.acceleration.x *0.5) * (dt * dt))
         self.px = self.rect.x + self.delpos.x
         self.rect.x = round(self.px)
-        hitx = pygame.sprite.spritecollide(player,platform_group, False)
+        hitx = pygame.sprite.spritecollide(self,platform_group, False)
         if hitx :
             if self.delpos.x > 0 :    
                 self.rect.right = hitx[0].rect.left
@@ -81,7 +100,7 @@ class Entity(pygame.sprite.Sprite) :
         self.velocity.y += self.acceleration.y
         self.delpos.y = self.velocity.y + (0.5 * self.acceleration.y)
         self.rect.y += self.delpos.y
-        hity = pygame.sprite.spritecollide(player, platform_group, False)
+        hity = pygame.sprite.spritecollide(self, platform_group, False)
         if hity :
             if self.delpos.y > 0 :
                 self.velocity.y = 0
@@ -97,7 +116,12 @@ class Entity(pygame.sprite.Sprite) :
         print(self.rect.topleft , self.position , self.velocity , self.acceleration , sep = "\t")
 
 run = True
-player = Entity()
+player = Entity("blue")
+pl = Entity("red")
+pl.color = "black"
+pl.move_right_key = pygame.K_d
+pl.move_left_key = pygame.K_a
+pl.jump_key = pygame.K_w
 border = border(0,0,1280,720)
 platform1 = Platform(0,690,1280,30,(255,20,0))
 platform2 = Platform(200, 600, 100, 50, (255,20,0))
@@ -112,16 +136,17 @@ platform5 = Platform(700, 600, 100, 50,(0,255,255))
 player_group = pygame.sprite.Group()
 platform_group = pygame.sprite.Group()
 platform_group.add(platform1,platform2,platform4,platform5)
-player_group.add(player)
+player_group.add(player , pl)
 
 
 
 while run :
     # clock.tick(120)
     dt = (clock.tick(60) /1000) * 60
-    player.debug()
+    # player.debug()
 
-    player.move()
+    pause()
+    player.move() ; pl.move()
 
     win.fill((255,255,255))
     platform_group.draw(win)
